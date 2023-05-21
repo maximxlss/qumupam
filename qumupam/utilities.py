66dc01989075899aba9696e33e659c290459606e
Maximxls <maximxlssoff@gmail.com>
2023-05-21 10:36:04 +0300

    aapt2 utilities

--- qumupam/utilities.py
+++ qumupam/utilities.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
+from pathlib import Path
 import re
-from subprocess import check_output
+from subprocess import CalledProcessError, check_output
 from enum import Enum
+import sys
 from typing import NamedTuple, Optional
 import inquirer as inq
+from tempfile import TemporaryDirectory
+import urllib.request
+
+
+AAPT2_PATH_ON_DEVICE = "/data/local/tmp/aapt2"
 
 
 class ADBStatus(Enum):
@@ -14,11 +21,11 @@ class ADBStatus(Enum):
 
 
 def run_cmd(cmd: list[str]) -> str:
-    return check_output(cmd, text=True, encoding="UTF-8")
+    return check_output(cmd, text=True, encoding="UTF-8", stderr=sys.stderr)
 
 
 def run_pm(cmd: list[str]) -> str:
-    return run_cmd(["adb", "shell", "pm", " ".join(cmd)])
+    return run_cmd(["adb", "shell", "pm", *cmd])
 
 
 def check_for_adb() -> ADBStatus:
@@ -49,6 +56,59 @@ def get_packages(uid=None, third_party_only=True) -> set[str]:
     packages = [s.removeprefix("package:") for s in pm_output.split()]
 
     return set(packages)
+
+
+def download_aapt2():
+    url = "https://github.com/rendiix/termux-aapt/raw/main/prebuilt-binary-android-12%2B/arm64/aapt2"
+
+    with urllib.request.urlopen(url) as f:
+        data = f.read()
+
+    with TemporaryDirectory() as tdir:
+        path = Path(tdir) / "aapt2"
+        path = str(path)
+        
+        with open(path, "wb") as f:
+            f.write(data)
+        
+        run_cmd(["adb", "push", path, AAPT2_PATH_ON_DEVICE])
+
+    run_cmd(["adb", "shell", "chmod", "+x", AAPT2_PATH_ON_DEVICE])
+
+
+def run_aapt2(cmd: list[str]) -> str:
+    return run_cmd(["adb", "shell", AAPT2_PATH_ON_DEVICE, *cmd])
+
+
+def check_aapt2_works() -> bool:
+    try:
+        run_aapt2(["-h"])
+        return True
+    except CalledProcessError as e:
+        return e.returncode == 1
+
+
+def get_apk_label(path) -> Optional[str]:
+    aapt2_out = run_aapt2(["dump", "badging", path]).split("\n")
+    
+    label_line = next((s for s in aapt2_out if s.startswith("application-label")), None)
+
+    if label_line is None:
+        return None
+
+    a = label_line.find("'")
+    b = label_line.find("'", a + 1)
+
+    return label_line[a + 1 : b]
+
+
+def get_apk_path(package) -> str:
+    return run_pm(["path", package]).strip().removeprefix("package:")
+
+
+def get_package_label(package) -> Optional[str]:
+    apk = get_apk_path(package)
+    return get_apk_label(apk)
 
 
 class User(NamedTuple):
